1e8409317c077bb0d69e3e937de2cc0e6a751269
sukacode <firmansahalhadi@gmail.com>
2023-02-22 11:38:13 +0700

    add-conv&data_type

--- src/PolymorphismApp.java
+++ src/PolymorphismApp.java
@@ -10,5 +10,21 @@ public class PolymorphismApp {
 
         employee = new VicePresident("cc");
         employee.sayHello("XX");
+
+        sayHello(new Employee("gg"));
+        sayHello(new VicePresident("hh"));
+        sayHello(new Manager("ii"));
+    }
+
+    static void sayHello(Employee employee){
+        if (employee instanceof VicePresident){
+            VicePresident vicePresident = (VicePresident) employee;
+            System.out.println("Hello VP " + vicePresident.name);
+        } else if (employee instanceof Manager){
+            Manager manager = (Manager) employee;
+            System.out.println("Hello Manager " + manager.name);
+        } else {
+            System.out.println("Hello " + employee.name);
+        }
     }
 }
